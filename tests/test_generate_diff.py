44b446091addfe5bbea61128a50735d34cf0778b
Andrey-Barinov <barbpro89@gmail.com>
2024-01-25 18:37:24 +0300

    fixed path

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,11 +2,11 @@ from gendiff.gendiff_json import generate_diff
 
 
 
-json_file1 = "/mnt/c/python-project-50/tests/fixtures/file1.json"
+json_file1 = "tests/fixtures/file1.json"
 
-json_file2 = "/mnt/c/python-project-50/tests/fixtures/file2.json"
+json_file2 = "tests/fixtures/file2.json"
 
-result_file = "/mnt/c/python-project-50/tests/fixtures/result_json_files.txt"
+result_file = "tests/fixtures/result_json_files.txt"
 
 
 def test_generate_diff():
