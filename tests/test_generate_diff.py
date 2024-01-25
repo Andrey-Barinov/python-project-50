5129e730b81073c9f16eef09e6c73eb475291404
Andrey-Barinov <barbpro89@gmail.com>
2024-01-25 18:31:29 +0300

    Fixed test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,15 +1,17 @@
 from gendiff.gendiff_json import generate_diff
-import pytest
 
 
-json_file1 = "tests/fixtures/file1.json"
 
-json_file2 = "tests/fixtures/file2.json"
+json_file1 = "/mnt/c/python-project-50/tests/fixtures/file1.json"
 
-result_file = "tests/fixtures/result_json_files.txt"
+json_file2 = "/mnt/c/python-project-50/tests/fixtures/file2.json"
+
+result_file = "/mnt/c/python-project-50/tests/fixtures/result_json_files.txt"
 
 
 def test_generate_diff():
     right_result = open(result_file, 'r')
     assert generate_diff(json_file1, json_file2) == right_result.read()
+
+test_generate_diff()
 
